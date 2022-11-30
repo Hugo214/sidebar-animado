aedc731b007de3c2cf8cefcba094d904e4839c85
Hugo214 <hugo39452@gmail.com>
2022-11-30 14:13:37 -0500

    sidebar

---
+++ main.js
@@ -0,0 +1,6 @@
+const btntoggle =  document.querySelector(".toggle-btn")
+
+btntoggle.addEventListener("click", function () {
+    document.getElementById("sidebar").classList.toggle("active");
+
+});
